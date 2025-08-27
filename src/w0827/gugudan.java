4d9c72f03b9e99315f63abb6235d36e6c13b0982
hyunhyesoo <sue990831@gmail.com>
2025-08-27 09:24:22 +0900

    이중 for문 연습(구구단)

--- src/w0827/gugudan.java
+++ src/w0827/gugudan.java
@@ -4,7 +4,8 @@ public class gugudan {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 2; j <= 9; j++) {
-                System.out.printf("%d * %d = %d\t", j, i, i * j); //줄바꿈 말고 tab 해줘야 가로 출력됨
+                System.out.printf("%d * %d = %d\t", j, i, i * j);
+                //줄바꿈 말고 tab 해줘야 가로 출력됨
             }
             System.out.println();
         }
