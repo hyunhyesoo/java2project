c0e88feca8aaaf4e69c763a7e5eb39e4b3db2547
hyunhyesoo <sue990831@gmail.com>
2025-09-29 16:35:16 +0900

    db 연결하기

---
+++ src/jdbc_test/JDBCConnector.java
@@ -0,0 +1,8 @@
+package jdbc_test;
+
+public class JDBCConnector {
+    private static final String DRIVER_PATH = "oracle.jdbc.driver.OracleDriver";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe"; //오라클 데베에 접근할 수 있는 주소
+    private static final String USER = "c##hhs";
+    private static final String PASSWORD = "1234";
+}
